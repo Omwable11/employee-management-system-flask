22d2f23ac98704f07983df07bc0e35ddb8d3ea01
Om Wable <omwable11@gmail.com>
2026-07-21 10:10:57 +0530

    Setup Flask application

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "<h1>Employee Management System</h1>"
+
+    return app
